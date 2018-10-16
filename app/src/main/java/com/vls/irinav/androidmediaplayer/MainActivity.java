dfe84e89c69ce62a8218594ac3bf2e7fd6ebfd78
Irina Vlasceanu <irinav0402@gmail.com>
2018-10-16 16:10:24 +0300

    User interface WIP

--- app/src/main/java/com/vls/irinav/androidmediaplayer/MainActivity.java
+++ app/src/main/java/com/vls/irinav/androidmediaplayer/MainActivity.java
@@ -1,13 +1,83 @@
 package com.vls.irinav.androidmediaplayer;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+//    private Button playButton;
+    private SeekBar mSeekbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer = MediaPlayer.create( getApplicationContext(), R.raw.vivaldi_sonata_eminor );
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+               int duration = mp.getDuration();
+               String mDuration = String.valueOf(duration/1000);
+
+               Toast.makeText( getApplicationContext(), "duration is " + mDuration, Toast.LENGTH_SHORT ).show();
+            }
+        });
+
+
+        mSeekbar = (SeekBar) findViewById(R.id.mSeekBar);
+        mSeekbar.setMax(mediaPlayer.getDuration());
+
+        mSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    mediaPlayer.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void pauseMusic() {
+        if ( mediaPlayer != null ) {
+            mediaPlayer.pause();
+        }
+    }
+
+    public void startMusic() {
+        if ( mediaPlayer != null ) {
+            mediaPlayer.start();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mediaPlayer !=  null && mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+
+        super.onDestroy();
     }
 }
+
+
