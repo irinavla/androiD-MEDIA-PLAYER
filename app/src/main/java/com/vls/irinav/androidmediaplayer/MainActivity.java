888b0aa065b49702b9361647a2c2e843bc2d5569
Irina Vlasceanu <irinav0402@gmail.com>
2018-10-17 11:13:28 +0300

    player final

--- app/src/main/java/com/vls/irinav/androidmediaplayer/MainActivity.java
+++ app/src/main/java/com/vls/irinav/androidmediaplayer/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ImageView artistImage;
     private TextView artistName, songName, timeLeft, timeRight;
     private Button prevButton, playButton, nextButton;
+    private Thread thread;
 
 
     @Override
@@ -39,6 +40,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     mediaPlayer.seekTo(progress);
                     updateSeekbar();
                 }
+
+                updateSeekbar();
 
             }
 
@@ -79,7 +82,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.prevButton:
-
+                backMusic();
                 break;
 
             case R.id.playButton:
@@ -91,7 +94,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             case R.id.nextButton:
-
+                nextMusic();
                 break;
         }
     }
@@ -106,8 +109,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void startMusic() {
         if (mediaPlayer != null) {
             mediaPlayer.start();
+            updateThread();
             playButton.setBackgroundResource(android.R.drawable.ic_media_pause);
-            updateSeekbar();
 
         }
     }
@@ -122,6 +125,67 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             timeLeft.setText(dateFormat.format( new Date(currentPos) ));
             timeRight.setText(dateFormat.format(new Date(duration)));
         }
+    }
+
+    public void updateThread() {
+        thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+
+                    while ( mediaPlayer != null && mediaPlayer.isPlaying() ) {
+                        Thread.sleep(50);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                int newPosition = mediaPlayer.getCurrentPosition();
+                                int newMax = mediaPlayer.getDuration();
+
+                                mSeekbar.setMax(newMax);
+                                mSeekbar.setProgress(newPosition);
+
+                                // update the text
+                                timeLeft.setText(String.valueOf( new SimpleDateFormat("mm:ss").format( new Date( mediaPlayer.getCurrentPosition()))));
+                                timeRight.setText( String.valueOf(
+                                        new SimpleDateFormat("mm:ss").format( new Date (mediaPlayer.getDuration() - mediaPlayer.getCurrentPosition()) )
+                                ));
+                            }
+                        });
+                    }
+
+                } catch ( InterruptedException e ) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+    }
+
+    public void backMusic() {
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.seekTo(0);
+            pauseMusic();
+        }
+    }
+
+    public void nextMusic() {
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.seekTo(mediaPlayer.getDuration() - 1000);
+            pauseMusic();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+
+        thread.interrupt();
+        thread = null;
+        super.onDestroy();
     }
 }
 
